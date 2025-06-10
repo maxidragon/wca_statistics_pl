c9b131e328287d58ab6c385c1b9d1ac95a94e5ab
Maksymilian Gala <maksymilian.gala1@gmail.com>
2025-06-10 11:17:27 +0200

    fix: avg results submission time

--- statistics/avg_results_submission_time.rb
+++ statistics/avg_results_submission_time.rb
@@ -3,25 +3,58 @@ require_relative "../core/statistic"
 class AvgResultsSubmissionTime < Statistic
   def initialize
     @title = "Average results submission time by Polish delegates"
-    @table_header = { "Delegate" => :right, "Average time" => :left}
+    @table_header = { "Delegate" => :right, "Average time" => :left, "Total delegated" => :right }
   end
 
   def query
     <<-SQL
-      SELECT
+SELECT
     CASE 
-    WHEN d.wca_id IS NOT NULL THEN CONCAT('[', d.name, '](https://www.worldcubeassociation.org/persons/', d.wca_id, ')')
-    ELSE d.name 
-    END as delegate_name,
-    AVG(TIMESTAMPDIFF(SECOND, (
-        SELECT sa.end_time
-        FROM schedule_activities sa
-        JOIN venue_rooms vr ON sa.venue_room_id = vr.id
-        JOIN competition_venues cv ON vr.competition_venue_id = cv.id
-        WHERE cv.competition_id = c.id
-        ORDER BY sa.end_time DESC
-        LIMIT 1
-    ), c.results_submitted_at) / 3600) AS avg_submission_time_hours
+        WHEN d.wca_id IS NOT NULL THEN CONCAT('[', d.name, '](https://www.worldcubeassociation.org/persons/', d.wca_id, ')')
+        ELSE d.name 
+    END AS delegate_name,
+    CASE 
+        WHEN ABS(AVG(TIMESTAMPDIFF(SECOND, (
+            SELECT sa.end_time
+            FROM schedule_activities sa
+            JOIN venue_rooms vr ON sa.venue_room_id = vr.id
+            JOIN competition_venues cv ON vr.competition_venue_id = cv.id
+            WHERE cv.competition_id = c.id
+            ORDER BY sa.end_time DESC
+            LIMIT 1
+        ), c.results_submitted_at) / 3600)) < 24 THEN
+            CONCAT(ROUND(AVG(TIMESTAMPDIFF(SECOND, (
+                SELECT sa.end_time
+                FROM schedule_activities sa
+                JOIN venue_rooms vr ON sa.venue_room_id = vr.id
+                JOIN competition_venues cv ON vr.competition_venue_id = cv.id
+                WHERE cv.competition_id = c.id
+                ORDER BY sa.end_time DESC
+                LIMIT 1
+            ), c.results_submitted_at) / 3600), 2), 'h')
+        ELSE
+            CONCAT(
+                FLOOR(AVG(TIMESTAMPDIFF(SECOND, (
+                    SELECT sa.end_time
+                    FROM schedule_activities sa
+                    JOIN venue_rooms vr ON sa.venue_room_id = vr.id
+                    JOIN competition_venues cv ON vr.competition_venue_id = cv.id
+                    WHERE cv.competition_id = c.id
+                    ORDER BY sa.end_time DESC
+                    LIMIT 1
+                ), c.results_submitted_at) / 3600) / 24), 'd ',
+                MOD(ROUND(AVG(TIMESTAMPDIFF(SECOND, (
+                    SELECT sa.end_time
+                    FROM schedule_activities sa
+                    JOIN venue_rooms vr ON sa.venue_room_id = vr.id
+                    JOIN competition_venues cv ON vr.competition_venue_id = cv.id
+                    WHERE cv.competition_id = c.id
+                    ORDER BY sa.end_time DESC
+                    LIMIT 1
+                ), c.results_submitted_at) / 3600), 2), 24), 'h'
+            )
+    END AS avg_submission_time,
+    COUNT(DISTINCT c.id) AS delegated_competitions
 FROM 
     competitions c
 JOIN 
@@ -31,11 +64,19 @@ JOIN
 WHERE 
     c.results_submitted_at IS NOT NULL
 GROUP BY 
-    d.id, d.name
+    d.id, d.name, d.wca_id
 HAVING 
-    avg_submission_time_hours IS NOT NULL
+    avg_submission_time IS NOT NULL
 ORDER BY 
-    avg_submission_time_hours ASC;
+    AVG(TIMESTAMPDIFF(SECOND, (
+        SELECT sa.end_time
+        FROM schedule_activities sa
+        JOIN venue_rooms vr ON sa.venue_room_id = vr.id
+        JOIN competition_venues cv ON vr.competition_venue_id = cv.id
+        WHERE cv.competition_id = c.id
+        ORDER BY sa.end_time DESC
+        LIMIT 1
+    ), c.results_submitted_at)) ASC;
     SQL
   end
 end

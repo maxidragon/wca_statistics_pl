b0a50bbee3127bd9eda8a42e34ca633edd1b95a4
Maksymilian Gala <maksymilian.gala1@gmail.com>
2026-06-29 23:40:45 +0200

    feat: rename stats to better reflect their content

--- statistics/most_attended_competitions_in_city.rb
+++ statistics/most_attended_competitions_in_city.rb
@@ -1,12 +1,9 @@
 require_relative "../core/grouped_statistic"
-require_relative "../core/voivodeships"
 
-class CompetitionsPerVoivodeship < GroupedStatistic
-  include Voivodeships
-
+class MostAttendedCompetitionsInCity < GroupedStatistic
   def initialize
-    @title = "Competitions per voivodeship"
-    @note = "Voivodeships are inferred from competition coordinates (approximate bounding box). "
+    @title = "Most attended competitions in a city"
+    @note = "Only Polish persons and Polish competitions with posted results are included."
     @table_header = { "Person" => :left, "Competitions" => :right }
   end
 
@@ -16,44 +13,39 @@ class CompetitionsPerVoivodeship < GroupedStatistic
         p.name,
         p.wca_id,
         c.id AS competition_id,
-        c.latitude / 1000000.0 AS lat,
-        c.longitude / 1000000.0 AS lon
+        c.city_name
       FROM results r
       JOIN persons p ON p.wca_id = r.person_id AND p.sub_id = 1 AND p.country_id = 'Poland'
       JOIN competitions c ON c.id = r.competition_id
       WHERE c.country_id = 'Poland'
         AND c.results_posted_at IS NOT NULL
         AND c.cancelled_at IS NULL
-        AND c.latitude IS NOT NULL
-        AND c.longitude IS NOT NULL
       GROUP BY p.wca_id, c.id
     SQL
   end
 
   def transform(results)
-    comp_ids_per_voiv = Hash.new { |h, k| h[k] = Set.new }
-    person_count_per_voiv = Hash.new { |h, k| h[k] = Hash.new(0) }
+    comp_ids_per_city = Hash.new { |h, k| h[k] = Set.new }
+    person_count_per_city = Hash.new { |h, k| h[k] = Hash.new(0) }
     person_links = {}
 
     results.each do |row|
-      voiv = voivodeship_for(row["lat"], row["lon"])
-      next unless voiv
-
+      city = row["city_name"]
       wca_id = row["wca_id"]
       person_links[wca_id] ||= "[#{row["name"]}](https://www.worldcubeassociation.org/persons/#{wca_id})"
-      comp_ids_per_voiv[voiv] << row["competition_id"]
-      person_count_per_voiv[voiv][wca_id] += 1
+      comp_ids_per_city[city] << row["competition_id"]
+      person_count_per_city[city][wca_id] += 1
     end
 
-    comp_ids_per_voiv
+    comp_ids_per_city
       .sort_by { |_, ids| -ids.size }
-      .map do |voiv, ids|
-        person_rows = person_count_per_voiv[voiv]
+      .map do |city, ids|
+        person_rows = person_count_per_city[city]
           .sort_by { |_, count| -count }
           .first(30)
           .map { |wca_id, count| [person_links[wca_id], count] }
 
-        header = "#{voivodeship_display_name(voiv)}\n_Total competitions: #{ids.size}_"
+        header = "#{city}\n_Total competitions: #{ids.size}_"
         [header, person_rows]
       end
   end

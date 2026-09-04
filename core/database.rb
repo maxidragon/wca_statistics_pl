dfbb9a92d510a16d657cefab8c67d6e7e901acb9
Maksymilian Gala <maksymilian.gala1@gmail.com>
2026-09-04 20:43:30 +0200

    feat: add index on result

--- core/database.rb
+++ core/database.rb
@@ -35,6 +35,7 @@ module Database
   REQUIRED_TABLES = DEV_TABLES + RESULTS_TABLES
   INDICES = [
     "CREATE INDEX index_results_on_competitionId_personId ON results (competition_id, person_id);",
+    "CREATE INDEX index_results_on_countryId_personId_competitionId ON results (country_id, person_id, competition_id, id);",
     "CREATE INDEX index_results_on_person_id ON results (person_id);",
     "CREATE INDEX index_result_attempts_on_result_id ON result_attempts (result_id);",
     "CREATE INDEX index_results_on_event_id ON results (event_id);",

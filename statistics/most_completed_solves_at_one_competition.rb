a790b6dcc248cef7b750bcbcba139577bc81d0fa
Maksymilian Gala <maksymilian.gala1@gmail.com>
2026-09-04 20:40:42 +0200

    feat: optimize most_completed_solves_at_one_competition

--- statistics/most_completed_solves_at_one_competition.rb
+++ statistics/most_completed_solves_at_one_competition.rb
@@ -8,22 +8,31 @@ class MostCompletedSolvesAtOneCompetition < Statistic
 
   def query
     <<-SQL
+      WITH person_competition_solves AS (
+        SELECT
+          r.person_id,
+          r.competition_id,
+          SUM(ra.value > 0) completed_count,
+          SUM(ra.value != 0) attempts_count
+        FROM results r
+        JOIN result_attempts ra ON ra.result_id = r.id
+        WHERE r.country_id = 'Poland'
+        GROUP BY r.person_id, r.competition_id
+        ORDER BY completed_count DESC, attempts_count ASC, r.person_id, r.competition_id
+        LIMIT 20
+      )
       SELECT
         CONCAT('[', person.name, '](https://www.worldcubeassociation.org/persons/', person.wca_id, ')') person_link,
         CONCAT('[', competition.cell_name, '](https://www.worldcubeassociation.org/competitions/', competition.id, ')') competition_link,
-        SUM(CASE WHEN ra.value > 0 THEN 1 ELSE 0 END) completed_count,
-        SUM(CASE WHEN ra.value != 0 THEN 1 ELSE 0 END) attempts_count
-      FROM results r
-      JOIN result_attempts ra ON ra.result_id = r.id
-      JOIN persons person 
-        ON person.wca_id = r.person_id 
+        pcs.completed_count,
+        pcs.attempts_count
+      FROM person_competition_solves pcs
+      JOIN persons person
+        ON person.wca_id = pcs.person_id
         AND person.sub_id = 1
-      JOIN competitions competition 
-        ON competition.id = r.competition_id
-      WHERE r.country_id = 'Poland'
-      GROUP BY person.wca_id, competition.id
-      ORDER BY completed_count DESC
-      LIMIT 20
+      JOIN competitions competition
+        ON competition.id = pcs.competition_id
+      ORDER BY pcs.completed_count DESC, pcs.attempts_count ASC, pcs.person_id, pcs.competition_id
     SQL
   end
 end
